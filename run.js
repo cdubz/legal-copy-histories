43a6cdfbf9429c7d6f8307467adbb5f44cf1c60c
Christopher C. Wells <git@chris-wells.net>
2020-10-28 15:29:36 -0700

    Add update source filtering

--- run.js
+++ run.js
@@ -5,11 +5,29 @@ const fs = require("fs");
 const https = require("https");
 const turndown = require("turndown");
 
+let sourceArg;
+const args = process.argv.slice(2);
+if (args.length !== 1) {
+  return console.error('Invalid argument(s). Use source name or "--all" to update all sources.');
+} else {
+  sourceArg = args[0];
+}
+
 fs.readFile("sources.json", (err, data) => {
   if (err) {
-    console.error(err);
+    return console.error(err);
   } else {
-    JSON.parse(data).map((source) => evaluateSource(source));
+    let sources = JSON.parse(data);
+    if (sourceArg === "--all") {
+      // Do nothing. Update all sources.
+    } else {
+      sources = sources.filter((source) => source.name === sourceArg)
+    }
+    if (sources.length > 0) {
+      sources.map((source) => evaluateSource(source));
+    } else {
+      return console.error(`Invalid source "${sourceArg}".`);
+    }
   }
 });
 
